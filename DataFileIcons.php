9e44c83a5da71fbdd37bc0275b67fafa120328eb
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-07-05 15:55:05 +0200

    Create DataFileIcons.php

---
+++ DataFileIcons.php
@@ -0,0 +1,88 @@
+<?php
+
+const FILE_ICONS = [
+	// Documents
+	"pdf" => "file-pdf",
+	"doc" => "file-word",
+	"dot" => "file-word",
+	"wbk" => "file-word",
+	"docx" => "file-word",
+	"docm" => "file-word",
+	"dotx" => "file-word",
+	"dotm" => "file-word",
+	"docb" => "file-word",
+	"xls" => "file-excel",
+	"xlt" => "file-excel",
+	"xlm" => "file-excel",
+	"xlsx" => "file-excel",
+	"xlsm" => "file-excel",
+	"xltx" => "file-excel",
+	"xltm" => "file-excel",
+	"ppt" => "file-powerpoint",
+	"pot" => "file-powerpoint",
+	"pps" => "file-powerpoint",
+	"pptx" => "file-powerpoint",
+	"pptm" => "file-powerpoint",
+	"potx" => "file-powerpoint",
+	"potm" => "file-powerpoint",
+	"ppam" => "file-powerpoint",
+	"ppsx" => "file-powerpoint",
+	"ppsm" => "file-powerpoint",
+	"sldx" => "file-powerpoint",
+	"sldm" => "file-powerpoint",
+
+	// Image
+	"tif" => "file-image",
+	"tiff" => "file-image",
+	"bmp" => "file-image",
+	"jpg" => "file-image",
+	"jpeg" => "file-image",
+	"gif" => "file-image",
+	"png" => "file-image",
+	"eps" => "file-image",
+	"svg" => "file-image",
+
+	// Audio
+	"wav" => "file-audio",
+	"aiff" => "file-audio",
+	"mp3" => "file-audio",
+	"aac" => "file-audio",
+	"ogg" => "file-audio",
+	"wma" => "file-audio",
+	"flac" => "file-audio",
+	"alac" => "file-audio",
+
+	// Video
+	"avi" => "file-video",
+	"flv" => "file-video",
+	"wmv" => "file-video",
+	"mov" => "file-video",
+	"mp4" => "file-video",
+
+	// Archive
+	"zip" => "file-archive",
+	"7z" => "file-archive",
+	"tar" => "file-archive",
+	"gz" => "file-archive",
+	"tgz" => "file-archive",
+	"bz2" => "file-archive",
+	"xz" => "file-archive",
+	"deb" => "file-archive",
+	"iso" => "file-archive",
+	"rar" => "file-archive",
+
+	// Code
+	"html" => "file-code",
+	"css" => "file-code",
+	"js" => "file-code",
+	"php" => "file-code",
+	"java" => "file-code",
+	"lua" => "file-code",
+	"sh" => "file-code",
+	"class" => "file-code",
+
+	// Misc.
+	"csv" => "file-csv",
+	"txt" => "file",
+	"md" => "file",
+];
