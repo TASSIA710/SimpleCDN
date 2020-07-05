aed1faceb7b71aec6933b2828d2255c67c142b02
Tassilo <38081490+TASSIA710@users.noreply.github.com>
2020-07-05 15:55:22 +0200

    Create DataFileTypes.php

---
+++ DataFileTypes.php
@@ -0,0 +1,34 @@
+<?php
+
+const FILE_TYPES = [
+	// Documents
+	"pptx" => "Microsoft PowerPoint Presentation",
+	"pdf" => "PDF Document",
+
+	// Image
+	"png" => "PNG Image",
+	"svg" => "SVG Image",
+
+	// Audio
+	"mp3" => "MP3 Sound",
+
+	// Video
+
+	// Archive
+	"zip" => "ZIP Archive",
+
+	// Code
+	"css" => "Cascading Stylesheet",
+	"js" => "JavaScript",
+	"php" => "PHP: Hypertext Preprocessor",
+	"java" => "Java Source",
+	"lua" => "Lua Script",
+	"sh" => "Shell Script",
+	"class" => "Java Bytecode",
+	"bluej" => "BlueJ Project",
+	"pkg" => "Package",
+
+	// Misc.
+	"md" => "Markdown",
+	"txt" => "Text File",
+];
